15797f83b7335b7fcb851690c5716daa2ac3f125
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-10-24 15:07:20 -0400

    Added test for createUser handler.

--- src/event-bus/create-user-handler.ts
+++ src/event-bus/create-user-handler.ts
@@ -2,9 +2,11 @@ import { handlerFor } from '@node-ts/bus-core';
 import { session } from '../repository/neo4j';
 import { CreateUserCommand } from './create-user-command';
 
-export const createUserHandler = handlerFor(CreateUserCommand, async (command) => {
+export async function handlerForCreateUser(command: CreateUserCommand) {
   await session.run('CREATE (:USER {name: $nameParam, email: $emailParam})', {
     nameParam: command.userCreation.name,
     emailParam: command.userCreation.email,
   });
-});
+}
+
+export const createUserHandler = handlerFor(CreateUserCommand, handlerForCreateUser);
