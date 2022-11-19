28e1788d9a5548af7e8d1a7419a185bde7ba4f82
Rodrigo Chaves <oxygeniumrodrigo@hotmail.com>
2022-11-19 15:49:31 -0400

    last updates

--- src/event-bus/hello-world-handler.ts
+++ src/event-bus/hello-world-handler.ts
@@ -8,69 +8,73 @@ export const helloWorldHandler = handlerFor(HelloWorldCommand, async (command) =
     // const token = login.headers.token;
     // console.log(login.data);
 
-    const personalAccessToken = '5jp3yhkz6t83ix6m65tcg9zncr';
+    const personalAccessToken = 'rks4a6rjb3nxtgfu7rg7wadxhy';
     const authHeader = `Bearer ${personalAccessToken}`;
     const baseURL = 'http://localhost:8065/api/v4';
 
+    // const response = await fetch(`${baseURL}/teams`, { headers: { Authorization: authHeader } });
+    // const total = await response.json();
+    // console.log(total);
+
     // const response = await fetch(
-    //   `${baseURL}/users/stats/filtered?in_team=4wz81jjtrpyktqt9fd638t1bdo&include_deleted=false&include_bots=false`,
+    //   `${baseURL}/users/stats/filtered?in_team=69xtmuhrxiyrmdwu6tmgr9cedw&include_deleted=false&include_bots=false`,
     //   { headers: { Authorization: authHeader } },
     // );
     // const total = await response.json();
     // console.log(total);
 
-    let response = await fetch(`${baseURL}/users?in_team=4wz81jjtrpyktqt9fd638t1bdo&active=true&page=0&per_page=2`, {
-      headers: { Authorization: authHeader },
-    });
-    const users = await response.json();
-    console.log(users);
+    // let response = await fetch(`${baseURL}/users?in_team=4wz81jjtrpyktqt9fd638t1bdo&active=true&page=0&per_page=2`, {
+    //   headers: { Authorization: authHeader },
+    // });
+    // const users = await response.json();
+    // console.log(users);
 
-    response = await fetch(`${baseURL}/users?in_team=4wz81jjtrpyktqt9fd638t1bdo&active=true&page=2&per_page=2`, {
-      headers: { Authorization: authHeader },
-    });
-    const users2 = await response.json();
-    console.log(users2);
+    // response = await fetch(`${baseURL}/users?in_team=4wz81jjtrpyktqt9fd638t1bdo&active=true&page=2&per_page=2`, {
+    //   headers: { Authorization: authHeader },
+    // });
+    // const users2 = await response.json();
+    // console.log(users2);
     // const users = await axiosConn.post('/users/usernames', ['feedback-taqtile-bot', 'rodrigochaves']);
     // // console.log(users.data);
 
-    // const directChannel = await axiosConn.post('/channels/direct', [users.data[0].id, users.data[1].id]);
-    // // console.log(directChannel.data);
+    const directChannel = await fetch('/channels/direct', [users.data[0].id, users.data[1].id]);
+    console.log(directChannel.data);
 
-    // const post = await axiosConn.post('/posts', {
-    //   channel_id: directChannel.data.id,
-    //   message: 'Hello, this is a testing post',
-    //   props: {
-    //     attachments: [
-    //       {
-    //         text: 'Hello, time for your feedback!',
-    //         actions: [
-    //           {
-    //             id: 'FeedbackRequest',
-    //             name: 'Request Feedback',
-    //             integration: {
-    //               url: 'https://82fb-179-253-169-177.sa.ngrok.io/helloworld',
-    //               context: {
-    //                 action: 'do_something_ephemeral',
-    //               },
-    //             },
-    //           },
-    //           {
-    //             id: 'update',
-    //             name: 'Update',
-    //             integration: {
-    //               url: 'https://82fb-179-253-169-177.sa.ngrok.io/helloworld',
-    //               context: {
-    //                 action: 'do_something_update',
-    //               },
-    //             },
-    //           },
-    //         ],
-    //       },
-    //     ],
-    //   },
-    // });
+    const post = await axiosConn.post('/posts', {
+      channel_id: directChannel.data.id,
+      message: 'Hello, this is a testing post',
+      // props: {
+      //   attachments: [
+      //     {
+      //       text: 'Hello, time for your feedback!',
+      //       actions: [
+      //         {
+      //           id: 'FeedbackRequest',
+      //           name: 'Request Feedback',
+      //           integration: {
+      //             url: 'https://82fb-179-253-169-177.sa.ngrok.io/helloworld',
+      //             context: {
+      //               action: 'do_something_ephemeral',
+      //             },
+      //           },
+      //         },
+      //         {
+      //           id: 'update',
+      //           name: 'Update',
+      //           integration: {
+      //             url: 'https://82fb-179-253-169-177.sa.ngrok.io/helloworld',
+      //             context: {
+      //               action: 'do_something_update',
+      //             },
+      //           },
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
+    });
 
-    // console.log(post.data);
+    console.log(post.data);
   } catch (error) {
     console.log(error);
   }
